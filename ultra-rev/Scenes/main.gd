80f0f4bade33b06c982d159c79776adfa9869b12
Baby-Spinach <jatin.craine@gmail.com>
2026-03-14 17:10:24 -0400

    Revised player camera movement
    -Made fov scale with forward velocity instead of just while sprinting
    -Added camera tilt for jumping and falling instead of just while walking

--- ultra-rev/Scenes/main.gd
+++ ultra-rev/Scenes/main.gd
@@ -4,5 +4,5 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	$PlayerWrap/Player.set_meta("fov", 90.0)
+	#$PlayerWrap/Player.set_meta("fov", 90.0)
 	pass # Replace with function body.
--- ultra-rev/Scripts/player.gd
+++ ultra-rev/Scripts/player.gd
@@ -1,5 +1,9 @@
+class_name Player
 extends CharacterBody3D
 
+@export var FOV : float = 90.0
+#var sprinting = false
+var double_jump = false
 
 const SPEED = 7.0
 const SPRINT_BOOST = 2.0
@@ -24,25 +28,40 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	# Fov stretch while sprinting
-	if get_meta("sprinting"):
-		$"Main Camera".fov = lerp($"Main Camera".fov, get_meta("fov") + 15, FOV_STRETCH_WEIGHT * delta * 120)
+	var v_forward = -velocity.rotated(Vector3.UP,-get_parent().rotation.y).z
+	# Fov raises exponentially towards +8 when forward velocity approches max sprint speed, 
+	# and decreases linearly as backward velocity increases
+	var fov_target = FOV + clamp(exp(log(9)*v_forward/14)-1 if v_forward >= 0 else log(9) * v_forward / 14, -15, 15)
+	if not is_zero_approx(velocity.z):
+		$"Main Camera".fov = lerp($"Main Camera".fov, fov_target, FOV_STRETCH_WEIGHT * delta * 120)
 	else:
-		$"Main Camera".fov = lerp($"Main Camera".fov, get_meta("fov"), FOV_STRETCH_WEIGHT * delta * 120)
+		$"Main Camera".fov = lerp($"Main Camera".fov, FOV, FOV_STRETCH_WEIGHT * delta * 120)
 
 
 func _physics_process(delta: float) -> void:
+	# Update player movement/velocity
+	_player_movement_update(delta)
+	if Input.is_action_pressed("grapple"):
+		_grapple(delta)
+	# Move player then reset wrapper position to the players location
+	move_and_slide()
+	var pos = global_position
+	get_parent().global_position = global_position-Vector3(0,0.5,0)
+	global_position = pos
+
+func _player_movement_update(delta: float) -> void:
 	# Add the gravity and resets double jump
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	else:
-		set_meta("doubleJump", true)
+		double_jump = true
 
 	# Handle jump and double jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	elif Input.is_action_just_pressed("ui_accept") and get_meta("doubleJump"):
+	elif Input.is_action_just_pressed("ui_accept") and double_jump:
 		velocity.y = JUMP_VELOCITY * 1.25
-		set_meta("doubleJump", false)
+		double_jump = false
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -54,9 +73,9 @@ func _physics_process(delta: float) -> void:
 	# Also boosts FOV while sprinting
 	if Input.is_action_pressed("sprint") and direction.dot(Vector3.FORWARD) > 0:
 		direction += (SPRINT_BOOST - 1) * direction.dot(Vector3.FORWARD) * Vector3.FORWARD
-		set_meta("sprinting", true)
-	else:
-		set_meta("sprinting", false)
+		#sprinting = true
+	#else:
+		#sprinting = false
 	
 	# Changing player velocity based on direction, and also handles acceleration
 	if direction:
@@ -64,22 +83,31 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = velocity.lerp(Vector3(0, velocity.y, 0), SPEED_WEIGHT * delta * ProjectSettings.get_setting("physics/common/physics_ticks_per_second"))
 	
-	# Camera lean for side to side movement
-	var target = -direction.x * PI/30
+	# Camera lean for side to side movement, maxes at 10 degrees in either direction
+	var target = clamp(-direction.x * PI/45, -PI/36, PI/36)
 	if direction.x:
 		get_parent().rotation = get_parent().rotation.lerp(Vector3(get_parent().rotation.x, get_parent().rotation.y, target), CAM_TILT_WEIGHT * delta * ProjectSettings.get_setting("physics/common/physics_ticks_per_second"))
 	else:
 		get_parent().rotation = get_parent().rotation.lerp(Vector3(get_parent().rotation.x, get_parent().rotation.y, 0), CAM_TILT_WEIGHT * delta * ProjectSettings.get_setting("physics/common/physics_ticks_per_second"))
 	
-	# Camera lean for front to back movement
-	target = direction.z * PI/30
-	if direction.z:
+	# Camera lean for front to back and vertical movement, maxes at 10 degrees for front to back, and 20 degrees for vertical
+	target = clamp(direction.z * PI/45, -PI/36, PI/36)
+	target += clamp(exp(-velocity.y*log(1+PI/18)/4.5)-1 if velocity.y>0 else -exp(velocity.y*log(1+PI/36)/4.5)+1, -PI/18, PI/18)
+	if direction.z or velocity.y:
 		get_parent().rotation = get_parent().rotation.lerp(Vector3(target, get_parent().rotation.y, get_parent().rotation.z), CAM_TILT_WEIGHT * delta * ProjectSettings.get_setting("physics/common/physics_ticks_per_second"))
 	else:
 		get_parent().rotation = get_parent().rotation.lerp(Vector3(0, get_parent().rotation.y, get_parent().rotation.z), CAM_TILT_WEIGHT * delta * ProjectSettings.get_setting("physics/common/physics_ticks_per_second"))
+
+
+
+func _grapple(delta: float) -> void:
+	var space_state : PhysicsDirectSpaceState3D = $"Main Camera".get_world_3d().direct_space_state
+	var screen_center : Vector2 = get_viewport().size/2
+	var origin : Vector3 = $"Main Camera".project_ray_origin(screen_center)
+	var end : Vector3 = origin + $"Main Camera".project_ray_normal(screen_center) * 20
+	var query := PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_bodies = true
+	var result := space_state.intersect_ray(query)
+	#if result["collider"] == StaticBody3D:
+		
 	
-	# Move player then reset wrapper position to the players location
-	move_and_slide()
-	var pos = global_position
-	get_parent().global_position = global_position-Vector3(0,0.5,0)
-	global_position = pos
